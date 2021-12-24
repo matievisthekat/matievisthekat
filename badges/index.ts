182ee13c07ba456748fee67ce2a7d01a921550cf
matievisthekat <matievisthekat@gmail.com>
2021-12-24 10:56:11 +0200

    set up parsing of ejs and displaying of badges

--- badges/index.ts
+++ badges/index.ts
@@ -1,22 +1,17 @@
 import {parse} from 'https://deno.land/std@0.119.0/encoding/yaml.ts';
-import {renderFile} from 'https://deno.land/x/dejs/mod.ts';
+import {renderFileToString} from 'https://deno.land/x/dejs/mod.ts';
 import {join, fromFileUrl, dirname} from 'https://deno.land/std/path/mod.ts';
-import {StringWriter} from 'https://deno.land/std/io/mod.ts';
-import {copy} from 'https://deno.land/std/streams/conversion.ts';
 import {ensureFile} from 'https://deno.land/std/fs/ensure_file.ts';
 
 const __dirname = dirname(fromFileUrl(import.meta.url));
 const rawYml = Deno.readTextFileSync(join(__dirname, 'badges.yml'));
 const badges = parse(rawYml);
 
-renderFile(join(__dirname, 'base.ejs'), {badges}).then(async (reader) => {
-  const writer = new StringWriter();
-  await copy(reader, writer);
-
+renderFileToString(join(__dirname, 'base.ejs'), {badges}).then(async (html) => {
   const filePath = join(__dirname, '../badges.svg');
   await ensureFile(filePath);
   
   const file = await Deno.open(filePath, {write: true});
-  await file.write(new TextEncoder().encode(writer.toString()));
+  await file.write(new TextEncoder().encode(html));
   file.close();
 });
